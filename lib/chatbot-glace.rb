171134f2cd02556d3093cc794740e5589e6b7903
ikramiste <ikrame.saadi@gmail.com>
2023-10-11 12:15:42 +0200

    first commit

---
+++ lib/chatbot-glace.rb
@@ -0,0 +1,32 @@
+require 'http'
+require 'dotenv'
+require 'json'
+
+Dotenv.load
+
+# création de la clé d'api et indication de l'url utilisée.
+
+api_key = ENV["OPENAI_API_KEY"]
+url = "https://api.openai.com/v1/engines/text-babbage-001/completions"
+
+headers = {
+  "Content-Type" => "application/json",
+  "Authorization" => "Bearer #{api_key}"
+}
+
+data = {
+  "prompt" => "5 parfums de glace",
+  "max_tokens" => 55,
+  "temperature" => 0.9
+}
+
+# une partie un peu plus complexe :
+# - cela permet d'envoyer les informations en json à ton url
+# - puis de récupéré la réponse puis de séléctionner spécifiquement le texte rendu
+response = HTTP.post(url, headers: headers, body: data.to_json)
+response_body = JSON.parse(response.body.to_s)
+response_string = response_body['choices'][0]['text'].strip
+
+# ligne qui permet d'envoyer l'information sur ton terminal
+puts "Voici 5 parfums de glace :"
+puts response_string
